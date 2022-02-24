94993da474fe96109b3ceb5c82ff913f34f8823f
Эдуард Гетман <getman.edik2016@yandex.ru>
2022-02-24 15:33:41 +0300

    Перенос профилей добавление классов для маппинга

---
+++ Application/Interfaces/IMapWith.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicQueue.Core.Application.Interfaces
+{
+    public interface IMapWith<T> where T : class
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
